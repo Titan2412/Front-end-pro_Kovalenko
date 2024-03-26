60939837757d5b27406dff6d6de84e494de61ad4
Titan2412 <andrii.kovalenko241@gmail.com>
2024-03-26 18:31:39 +0200

    Completed homework 1

---
+++ homework_1/task_1_Hello_user/script.js
@@ -0,0 +1,7 @@
+alert("hello");
+
+const name = prompt("What is your name?");
+const country = prompt("Where are you from?");
+const message = "Nice to meet you," + " " + name + "! You are from" + " " + country + "!";
+
+alert(message);
