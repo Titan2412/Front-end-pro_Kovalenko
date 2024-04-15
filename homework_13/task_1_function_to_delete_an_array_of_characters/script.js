1f334c9345687e8aeeaf883e214ec18a2333089b
Titan2412 <andrii.kovalenko241@gmail.com>
2024-04-15 18:04:08 +0300

    Homework 13 completed

--- homework_13/task_1_function_to_delete_an_array_of_characters/script.js
+++ homework_13/task_1_function_to_delete_an_array_of_characters/script.js
@@ -2,8 +2,12 @@ const message = prompt('Enter yor strinng');
 const chars = prompt('Enter some chars for removing');
 
 function foo(string, arr) {
-    // yor awesome code
-    return
+    for(let i of arr) {
+        for(let j = 0; j < string.length; j++) {
+            string = string.replace(i, '')
+        }
+    }
+    return string
 }
 
 const result = foo(message, chars);
