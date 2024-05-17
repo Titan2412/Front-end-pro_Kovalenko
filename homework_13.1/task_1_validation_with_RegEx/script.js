52116911e58c905132a4a726811c8edff24fa74d
Titan2412 <andrii.kovalenko241@gmail.com>
2024-05-17 15:56:43 +0300

    Fixing homework 13.1

--- homework_13.1/task_1_validation_with_RegEx/script.js
+++ homework_13.1/task_1_validation_with_RegEx/script.js
@@ -1,9 +1,9 @@
 const btnSubmit = document.getElementById("btn-submit");
-const userName = document.getElementById("userName");
-const userMessage = document.getElementById("userMessage");
-const userPhone = document.getElementById("userPhone");
-const userEmail = document.getElementById("userEmail");
-const nameError = document.getElementById('nameError') 
+const userName = document.forms.action.elements.userName;
+const userMessage = document.forms.action.elements.userMessage;
+const userPhone = document.forms.action.elements.userPhone;
+const userEmail = document.forms.action.elements.userEmail;
+const nameError = document.getElementById('nameError');
 const messageError = document.getElementById('messageError') 
 const phoneError = document.getElementById('phoneError') 
 const emailError = document.getElementById('emailError') 
@@ -12,51 +12,35 @@ const validationMessage = /.{5}/;
 const validationPhone = /^\+380\d{9}/;
 const validationEmail = /\w+\@\w+\.\w+/;
 
+function chec(valid, el, erorrs) {
+    if(valid.test(el.value)) {
+        erorrs.style.display = 'none';
+        el.style.border = '1px solid white'
+        return true
+    } else {
+        erorrs.style.display = 'block';
+        el.style.border = '1px solid red'
+        return false
+    }
+}
+
 document.forms.action.addEventListener('submit', (e) => {
-    const result = {}
 
     e.preventDefault();
 
-    if(validationName.test(userName.value)) {
-        nameError.style.display = 'none';
-        userName.style.border = '1px solid white'
-    } else {
-        nameError.style.display = 'block';
-        userName.style.border = '1px solid red'
-        return false
+    const validName = chec(validationName, userName, nameError)
+    const validMessage = chec(validationMessage, userMessage, messageError)
+    const validPhone = chec(validationPhone, userPhone, phoneError)
+    const valiEmail = chec(validationEmail, userEmail, emailError)
+
+    if(validName && validMessage && validPhone && valiEmail) {
+        const result = {}
+
+        Array.from(e.target.elements).forEach( el => {
+            result[el.name] = el.value
+        })
+    
+        console.log(result)
     }
-
-    if(validationMessage.test(userMessage.value)) {
-        messageError.style.display = 'none';
-        userMessage.style.border = '1px solid white'
-    } else {
-        messageError.style.display = 'block';
-        userMessage.style.border = '1px solid red'
-        return false
-    }
-
-    if(validationPhone.test(userPhone.value)) {
-        phoneError.style.display = 'none';
-        userPhone.style.border = '1px solid white'
-    } else {
-        phoneError.style.display = 'block';
-        userPhone.style.border = '1px solid red'
-        return false
-    }
-
-    if(validationEmail.test(userEmail.value)) {
-        emailError.style.display = 'none';
-        userEmail.style.border = '1px solid white'
-    } else {
-        emailError.style.display = 'block';
-        userEmail.style.border = '1px solid red'
-        return false
-    }
-
-    Array.from(e.target.elements).forEach( el => {
-        result[el.name] = el.value
-    })
-
-    console.log(result)
 })
 
