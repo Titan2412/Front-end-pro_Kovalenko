91dd6896bd21f4d0f796842bc7b0f090443aacfe
Titan2412 <andrii.kovalenko241@gmail.com>
2024-05-28 11:46:26 +0300

    Fixing homework 18.1

--- homework_18.1/task_1_countdown_timer/script.js
+++ homework_18.1/task_1_countdown_timer/script.js
@@ -1,19 +1,38 @@
 const userTime = document.querySelector('[data-timer]');
-const timer = document.querySelector('.timers');
+const timer = document.querySelector('.user-timers');
 
 function startTime(duration) {
     let time = duration;
-    const timers = document.createElement("div");
-    timer.appendChild(timers);
+    let paused = false;
 
-    timerInterval = setInterval(() => {
+    const timerWrap = document.createElement('div');
+    timer.appendChild(timerWrap);
+    timerWrap.classList.add('timer-wrap')
+
+    const timers = document.createElement("div");
+    timerWrap.appendChild(timers);
+    timers.classList.add('timer');
+
+    const pause = document.createElement('button');
+    timerWrap.appendChild(pause)
+    pause.textContent = "Пауза"
+    pause.classList.add('pause');
+
+    let timerInterval = setInterval(() => {
         timers.textContent = timeFormat(time);
         if(time > 0) {
             time--
         } else {
             clearInterval(timerInterval);
         }
-    }, 1000)
+    }, 1000);
+
+    pause.addEventListener('click', () => {
+        if(!paused) {
+            clearInterval(timerInterval);
+            paused = false;
+        }
+    })
 }
 
 function timeFormat(sec) {
@@ -27,6 +46,15 @@ document.querySelector('[data-go]').addEventListener('click', () => {
     const inputValue = userTime.value.split(':');
     const minutes = parseInt(inputValue[0]);
     const seconds = parseInt(inputValue[1]);
-    const duration = minutes * 60 + seconds;
-    startTime(duration);
+    const error = document.querySelector(".error");
+
+    if (isNaN(minutes) || isNaN(seconds)) {
+        error.style.display = 'block';
+    } else {
+        error.style.display = 'none';
+        const duration = minutes * 60 + seconds;
+        startTime(duration);
+    }
+    
+    userTime.value = ''
 })
